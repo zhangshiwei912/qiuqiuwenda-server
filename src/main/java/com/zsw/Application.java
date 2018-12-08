28e8c8923c95d233dbce21eb2a07873eabda6262
zhangshiwei912 <1784636772@qq.com>
2018-12-08 22:53:50 +0800

    第一次提交

---
+++ src/main/java/com/zsw/Application.java
@@ -0,0 +1,29 @@
+
+package com.zsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zsw on 2018/11/16.
+ */
+
+@Controller
+@SpringBootApplication
+@ComponentScan
+@ImportResource({"classpath*:applicationContext.xml"})
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+}
